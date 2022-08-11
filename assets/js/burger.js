c9767976d12c00b7d416cfd2bcdcc495a59f76c1
MohamedMAGHRAOUI <maghraoui95000@me.com>
2022-08-11 14:02:19 +0200

    dark mode daltonien

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -9,11 +9,13 @@ function toggleMenu () {
 
 
   function toggleButton () { 
+
   const body = document.querySelector('body')
   const toggle = document.getElementById('toggle');
   toggle.onclick = function(){
     toggle.classList.toggle('active');
     body.classList.toggle('active');
+    
   }}
 
   toggleButton();
